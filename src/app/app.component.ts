16343f76bf006264def5b3d8599704b128ecbcf9
“Thomas <thomas_vracer2@hotmail.com>
2020-05-13 12:38:24 +0200

    Contact list + add form added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'bdtraining';
+  contacts: Contact[] = [
+    {firstName: 'Fred', surname: 'de Boer', email: 'fredje1966@hotmail.com'},
+    {firstName: 'Jannie', surname: 'Bijvens', email: 'superjannie@yahoo.com'},
+    {firstName: 'Bert', surname: 'de Boer', email: 'bertversebroodjes@live.nl'}
+  ];
+  newContact = {} as Contact;
+
+  addContact() {
+    this.contacts.push(this.newContact);
+  }
+
 }
+
+
